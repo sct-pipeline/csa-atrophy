33a740518e74e9fbc73d557e00bc80e9d5a322cc
Paul <pbautin70@gmail.com>
2020-05-14 10:49:38 -0400

    resampling to multiple values: modified process_data.sh

--- process_data.sh
+++ process_data.sh
@@ -71,63 +71,46 @@ segment_if_does_not_exist(){
 # ==============================================================================
 # Go to results folder, where most of the outputs will be located
 cd $PATH_RESULTS
-
-# Create 2 directories one for original data and the other for data going to be resampled
-mkdir -p original
-mkdir -p resampled
-
 # Copy ###source images
-cp -r $PATH_DATA/${SUBJECT} original
-cp -r $PATH_DATA/${SUBJECT} resampled
+cp -r $PATH_DATA/${SUBJECT} $PATH_RESULTS
+cd $SUBJECT
 
 
-# T2w Original
+# T2w resampling
 # =============================================================================
-cd original/${SUBJECT}/anat
-file_t2=${SUBJECT}_T2w
-# Segment spinal cord (only if it does not exist)
-segment_if_does_not_exist $file_t2 "t2"
-file_t2_seg=$FILESEG
-# Create a close mask around the spinal cord for more accurate registration
-#sct_create_mask -i $file_t2 -p centerline,$file_t2_seg -size 35mm -f cylinder -o mask_t2.nii.gz
-# Create labels in the cord at C1 and C3 upper cervical vertebral levels (only if it does not exist)
-label_if_does_not_exist $file_t2 $file_t2_seg
-file_label=$FILELABEL
-# Register to template
-sct_register_to_template -i $file_t2.nii.gz -s $file_t2_seg.nii.gz -l $file_label.nii.gz -c t2 -param step=1,type=seg,algo=centermassrot:step=2,type=im,algo=syn,iter=5,slicewise=1,metric=CC,smooth=0 -qc $PATH_QC
-# Warp template
-# Note: we don't need the white matter atlas at this point, therefore use flag "-a 0"
-sct_warp_template -d $file_t2.nii.gz -w warp_template2anat.nii.gz -a 0 -ofolder label_T2w -qc $PATH_QC
-# Compute average CSA between C1 and C3 levels (append across subjects)
-sct_process_segmentation -i $file_t2_seg.nii.gz -vert 1:3 -vertfile label_T2w/template/PAM50_levels.nii.gz -o $PATH_RESULTS/CSA.csv -append 1 -qc $PATH_QC
-# sct_process_segmentation -i $file_t2_seg.nii.gz -vert 1:3 -perslice 1 -vertfile label_T2w/template/PAM50_levels.nii.gz -o $PATH_RESULTS/CSA_perslice.csv -append 1 -qc $PATH_QC
 
-
-# T2w Resampled
-# =============================================================================
-cd ../../../
-cd resampled/$SUBJECT/anat
-# Image homothetic x0.9 rescaling
-sct_resample -i ${SUBJECT}_T2w.nii.gz -f 0.9x0.9x0.9
-
-file_t2_r=${SUBJECT}_T2w_r
-# Segment spinal cord (only if it does not exist)
-segment_if_does_not_exist $file_t2_r "t2"
-file_t2_seg_r=$FILESEG
-# Create a close mask around the spinal cord for more accurate registration
-#sct_create_mask -i $file_t2_r.nii.gz -p centerline,$file_t2_seg_r.nii.gz -size 35mm -f cylinder -o mask_t2_r.nii.gz
-# Create labels in the cord at C1 and C3 upper cervical vertebral levels (only if it does not exist)
-label_if_does_not_exist $file_t2_r $file_t2_seg_r
-file_label_r=$FILELABEL
-# Register to template
-sct_register_to_template -i $file_t2_r.nii.gz -s $file_t2_seg_r.nii.gz -l $file_label_r.nii.gz -c t2 -param step=1,type=seg,algo=centermassrot:step=2,type=im,algo=syn,iter=5,slicewise=1,metric=CC,smooth=0 -qc $PATH_QC
-# Warp template
-# Note: we don't need the white matter atlas at this point, therefore use flag "-a 0"
-sct_warp_template -d $file_t2_r.nii.gz -w warp_template2anat.nii.gz -a 0 -ofolder label_T2w -qc ${PATH_QC}
-# Compute average CSA between C1 and C3 levels (append across subjects)
-sct_process_segmentation -i $file_t2_seg_r.nii.gz -vert 1:3 -vertfile label_T2w/template/PAM50_levels.nii.gz -o $PATH_RESULTS/CSA_r.csv -append 1 -qc ${PATH_QC}
-# sct_process_segmentation -i $file_t2_seg_r.nii.gz -vert 1:3 -perslice 1 -vertfile label_T2w/template/PAM50_levels.nii.gz -o $PATH_RESULTS/CSA_perslice_r.csv -append 1 -qc ${PATH_QC}
-
+# define resamplling coefficients
+R_COEFS=(0.85 0.9 0.95 1)
+# iterate resample on subject
+for r_coef in ${R_COEFS[@]}; do
+  # rename anat to explicit resampling coefficient
+  mv anat anat_r$r_coef
+  cd anat_r${r_coef}
+  # Image homothetic rescaling
+  sct_resample -i ${SUBJECT}_T2w.nii.gz -f ${r_coef}x${r_coef}x${r_coef}
+  # rename file to explicit resampling
+  mv ${SUBJECT}_T2w_r.nii.gz ${SUBJECT}_T2w_r${r_coef}.nii.gz
+  file_t2=${SUBJECT}_T2w_r${r_coef}
+  # Segment spinal cord (only if it does not exist)
+  segment_if_does_not_exist $file_t2 "t2"
+  # name segmented file
+  file_t2_seg=$FILESEG
+  # Create a close mask around the spinal cord for more accurate registration
+  # sct_create_mask -i $file_t2_r.nii.gz -p centerline,$file_t2_seg_r.nii.gz -size 35mm -f cylinder -o mask_t2_r.nii.gz
+  # Create labels in the cord at C1 and C3 upper cervical vertebral levels (only if it does not exist)
+  label_if_does_not_exist $file_t2 $file_t2_seg
+  file_label=$FILELABEL
+  # Register to template
+  sct_register_to_template -i $file_t2.nii.gz -s $file_t2_seg.nii.gz -l $file_label.nii.gz -c t2 -param step=1,type=seg,algo=centermassrot:step=2,type=im,algo=syn,iter=5,slicewise=1,metric=CC,smooth=0 -qc $PATH_QC
+  # Warp template
+  # Note: we don't need the white matter atlas at this point, therefore use flag "-a 0"
+  sct_warp_template -d $file_t2.nii.gz -w warp_template2anat.nii.gz -a 0 -ofolder label_T2w -qc ${PATH_QC}
+  # Compute average CSA between C1 and C3 levels (append across subjects)
+  sct_process_segmentation -i $file_t2_seg.nii.gz -vert 1:3 -vertfile label_T2w/template/PAM50_levels.nii.gz -o $PATH_RESULTS/csa_r_${r_coef}.csv -append 1 -qc ${PATH_QC}
+  # sct_process_segmentation -i $file_t2_seg_r.nii.gz -vert 1:3 -perslice 1 -vertfile label_T2w/template/PAM50_levels.nii.gz -o $PATH_RESULTS/CSA_perslice_r.csv -append 1 -qc ${PATH_QC}
+  cd ../
+  cp -r $PATH_DATA/${SUBJECT}/anat .
+done
 
 
 # Verify presence of output files and write log file if error
@@ -140,16 +123,3 @@ for file in ${FILES_TO_CHECK[@]}; do
     echo "$SUBJECT/$file does not exist" >> $PATH_LOG/error.log
   fi
 done
-
-# stats correlation of original and resampled
-# =============================================================================
-
-#if
-cd ../../../../
-# python stat script goes with results
-cp -r CSA_rescale_stat.py ./results
-# use sct envionment
-cd results
-# Change {SCT_DIR} if sct directory is not spinalcordtoolbox
-source {SCT_DIR}/python/etc/profile.d/conda.sh
-conda activate venv_sct
