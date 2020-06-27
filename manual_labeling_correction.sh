c55310eb675c5ef8d8f8f39d5e06f2982f0b7c6d
Paul <pbautin70@gmail.com>
2020-06-27 01:04:20 -0400

    - manual labeling file path correction

--- manual_labeling_correction.sh
+++ manual_labeling_correction.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Local folder to output the manual labels
-PATH_SEGMANUAL="results_corrected/seg_manual"
+PATH_SEGMANUAL="csa-atrophy_results_corrected/seg_manual"
 mkdir -p ${PATH_SEGMANUAL}
 # List of subjects to create manual labels
 FILES=(
@@ -11,5 +11,5 @@ for file in ${FILES[@]}; do
   subject=${file%%_*}
   rescaling_split=${file%%_t*}
   rescaling=${rescaling_split#*_r}
-  sct_label_utils -i results/$subject/anat_r${rescaling}/$file -create-viewer 2,3,4,5,6 -o ${PATH_SEGMANUAL}/${file%%.nii.gz}_labels-manual.nii.gz -msg "Click at the posterior tip of inter-vertebral disc"
+  sct_label_utils -i csa-atrophy_results/results/$subject/anat_r${rescaling}/$file -create-viewer 2,3,4,5,6 -o ${PATH_SEGMANUAL}/${file%%.nii.gz}_labels-manual.nii.gz -msg "Click at the posterior tip of inter-vertebral disc"
 done
