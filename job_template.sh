9511c21330a5f52b365e948324e04f41dc50f92f
Sandrine Bedard <sandrine.bedard@polymtl.ca>
2024-08-13 17:35:19 -0400

    setup for compute canada

--- job_template.sh
+++ job_template.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=32    # number of OpenMP processes
 #SBATCH --mem=128G
-#SBATCH --mail-user=paul.bautin@polymtl.ca
+#SBATCH --mail-user=sandrine.bedard@polymtl.ca
 #SBATCH --mail-type=ALL
 export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
